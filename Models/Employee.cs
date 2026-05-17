7f11306e3c129e1bb6dd975cd02154829e4a7f18
Denz14-RDI <denzel14college@gmail.com>
2026-05-17 15:20:26 +0800

    Add project files.

---
+++ Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeCRUDApp.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }      // Primary key, auto-increment
+        public string? Name { get; set; }         // Employee's full name
+        public string? Position { get; set; }     // Job title
+        public string? Department { get; set; }   // Department name
+        public decimal Salary { get; set; }      // Salary amount
+    }
+}
---
+++ Models/EmployeeDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EmployeeCRUDApp.Models;
+
+namespace EmployeeCRUDApp.Models
+{
+    public class EmployeeDBContext : DbContext
+    {
+        public EmployeeDBContext(DbContextOptions<EmployeeDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
